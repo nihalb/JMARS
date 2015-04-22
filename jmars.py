f14f1d3b23557be7839aa565071effbb7a6aa13f
Revant Kumar <revant12@gmail.com>
2015-04-21 21:01:09 -0400

    Gradients for ruma added

--- jmars.py
+++ jmars.py
@@ -49,6 +49,41 @@ def func(params, *args):
 
     return total_loss
 
+def fprime(params, *args):
+    y = args[0]
+    z = args[1]
+    s = args[2]
+    Nums = args[3]
+    Numas = args[4]
+    Numa = args[5]
+
+    v_u = params[:(U*K)].reshape((U,K), order='F')
+    b_u = params[(U*K):(U*K + U)].reshape((U,1), order='F')
+    theta_u = params[(U*K + U):(U*K + U + U*A)].reshape((U,A), order='F')
+
+    v_m = params[(U*K + U + U*A):(U*K + U + U*A + M*K)].reshape((M,K), order='F')
+    b_m = params[(U*K + U + U*A + M*K):(U*K + U + U*A + M*K + M)].reshape((M,1), order='F')
+    theta_m = params[(U*K + U + U*A + M*K + M):(U*K + U + U*A + M*K + M + M*A)].reshape((M,A), order='F')
+
+    M_a = params[(U*K + U + U*A + M*K + M + M*A):].reshape((A,K), order='F')
+
+    M_sum = np.diag(M_a.sum(0))
+
+    #partial derivatives of ruma
+    grad_ruma_vu = np.zeros((U,M,A,K))
+    grad_ruma_vm = np.zeros((M,U,A,K))
+    for i in range(A):
+        grad_ruma_vu[:,:,i,:] = np.tile(np.multiply(np.matlib.repmat(M_a[i],M,1), v_m).reshape(M,1,K), (U,1,1,1))
+        grad_ruma_vm[:,:,i,:] = np.tile(np.multiply(np.matlib.repmat(M_a[i],U,1), v_u).reshape(U,1,K), (M,1,1,1))
+
+    grad_ruma_bu = 1
+    grad_ruma_bm = 1
+    grad_ruma_thetaua = 0
+    grad_ruma_thetama = 0
+    grad_ruma_mak =    np.tile(np.multiply(np.matlib.repmat(M_a[i],U,1), v_u).reshape(U,1,K), (M,1,1,1))
+
+
+
 
 #params = [v_u, b_u, theta_u, v_m, b_m, theta_m, M_a]
 args = (y,z,s,Nums,Numas,Numa)
