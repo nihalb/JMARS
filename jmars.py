e683e257fd2f3d3de9968cc3a19edfb3dc5ba5dd
Revant Kumar <revant12@gmail.com>
2015-04-26 01:42:02 -0400

    Adding predicted rating to jmars.py

--- jmars.py
+++ jmars.py
@@ -1,7 +1,7 @@
 import logging
 from constants import *
 from optimize import optimizer
-from sampler import GibbsSampler
+from sampler import GibbsSampler, predicted_rating
 import numpy as np
 import scipy as sp
 from scipy.optimize import fmin_l_bfgs_b
@@ -11,7 +11,7 @@ from indexer import Indexer
 
 # Constants
 MAX_ITER = 500
-MAX_OPT_ITER = 50
+MAX_OPT_ITER = 10
 
 def main():
     """
@@ -30,10 +30,10 @@ def main():
     rating_matrix, review_matrix, review_map) = imdb.get_mappings()
 
     # Get number of users and movies
-    U = len(user_list)
-    M = len(movie_list)
-    logging.info('No. of users U = %d' % U)
-    logging.info('No. of movies M = %d' % M)
+    Users = len(user_list)
+    Movies = len(movie_list)
+    logging.info('No. of users U = %d' % Users)
+    logging.info('No. of movies M = %d' % Movies)
 
     # Run Gibbs EM
     for it in xrange(1,MAX_ITER+1):
@@ -44,6 +44,12 @@ def main():
         logging.info('Running M-Step - Gradient Descent')
         for i in xrange(1,MAX_OPT_ITER+1):
             optimizer()
+
+    #Output Predicted Ratings
+    for u in range(U):
+        for m in range(M):
+            pred_rate = predicted_rating(u, m)
+            print "Predicted Rating of user " + str(u) + " and movie " + str(m) + ": " + str(pred_rate)
 
 if __name__ == "__main__":
     main()
